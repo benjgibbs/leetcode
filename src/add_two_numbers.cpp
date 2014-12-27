9f59f11a4113db611d4d90c2597361b19fbe78e0
Ben Gibbs <benjgibbs@gmail.com>
2014-12-27 18:18:38 +0000

    FASTER

--- src/add_two_numbers.cpp
+++ src/add_two_numbers.cpp
@@ -7,40 +7,36 @@ struct ListNode {
      ListNode(int x) : val(x), next(nullptr) {}
 };
 
-long long sum(ListNode * l){
-	long long sum = 0;
-	long long factor = 1;
-	while(l){
-		sum += factor * l->val;
-		factor *= 10;
-		l = l->next;
-	}
-	return sum;
-}
-
-
-ListNode* fromNum(long long num){
-	ListNode* head = nullptr;
-	ListNode* n1 = nullptr;
-	do{
-		ListNode* n2 = new ListNode(num%10);
-		num = num / 10;
-		if(head == nullptr) {
-			head = n2;
-			n1 = n2;
-		} else {
-			n1->next = n2;
-			n1 = n2; 
-		}
-	} while(num > 0); 
-	return head;
-}
-
 ListNode* addTwoNumbers(ListNode *l1, ListNode *l2) {
-	long long s1 = sum(l1);
-	long long s2 = sum(l2);	
+  ListNode* head = nullptr;
+  ListNode* cur = nullptr;
+  int carry = 0;
+  while(l1 || l2 || carry > 0) {
+    int l1val = 0;
+    int l2val = 0;
+    if(l1) { 
+      l1val = l1->val;
+      l1 = l1->next;
+    }
+    if(l2) {
+      l2val = l2->val;
+      l2 = l2->next;
+    }
+    int val = l1val + l2val + carry;
+    carry = val / 10;
+    ListNode* tmp = new ListNode(val % 10);
+     
+    if(head) {
+      cur->next = tmp;
+      cur = tmp;
+    } else {
+      head = tmp;
+      cur = tmp;
+    }
+    
+  }
 	
-  return fromNum(s1+s2);  
+  return head;  
 }
 
 ListNode* buildList(){
@@ -69,6 +65,7 @@ void print(ListNode* l){
 int main(int argc, char** argv){
 	print(addTwoNumbers(buildList(2,4,3),buildList(5,6,4)));
 	print(addTwoNumbers(buildList(0),buildList(0)));
+	print(addTwoNumbers(buildList(9),buildList(1,9,9,9,9,9,9,9,9,9)));
 	print(addTwoNumbers(buildList(9),buildList(1,9,9,9,9,9,9,9,9,9)));
 /*	
 	print(buildList(1,2,3,4));
