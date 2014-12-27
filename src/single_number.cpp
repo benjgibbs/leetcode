324b826cb1bfdf6948318151711e69b9feb1c871
Ben Gibbs <benjgibbs@gmail.com>
2014-12-27 23:39:54 +0000

    make faster with xor

--- src/single_number.cpp
+++ src/single_number.cpp
@@ -1,19 +1,12 @@
 
 #include <iostream>
-#include <vector>
-#include <unordered_map>
 
 int singleNumber(int A[], int n) {
-  std::unordered_multimap<int,int> m;
-
-  for(int i = 0; i < n; i++){
-    m.insert(std::make_pair(A[i],i));
+  int result = A[0];
+  for(int i = 1; i < n; i++){
+    result = result ^ A[i];
   }
-  for(auto l : m) {
-    if(m.count(l.first) == 1)
-      return l.first;
-  }
-  throw std::invalid_argument("no single number");
+  return result;
 }
 
 
@@ -24,6 +17,8 @@ void test(int A[], int n) {
 int main(int argc, char** argv) {
   int a1[] ={ 1, 1, 2, 2, 3 };
   test(a1, sizeof(a1)/sizeof(a1[0]) );
-  int a1[] ={ 1,  2, 2, 3, 3 };
-  test(a1, sizeof(a1)/sizeof(a1[0]) );
+  int a2[] ={ 1,  2, 2, 3, 3 };
+  test(a2, sizeof(a2)/sizeof(a2[0]) );
+  int a3[] ={ 1,3,1,-1,3 };
+  test(a3, sizeof(a3)/sizeof(a3[0]) );
 }
